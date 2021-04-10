fb2a29c3b2e368cbba72b2c2ef2004be6a04d5ae
l3nnartt <loesche.lennart@gmail.com>
2021-04-10 22:06:44 +0200

    Heal Command
    First Version of Heal Command

--- src/main/java/com/github/l3nnartt/plugin/CommandHeal.java
+++ src/main/java/com/github/l3nnartt/plugin/CommandHeal.java
@@ -3,11 +3,32 @@ package com.github.l3nnartt.plugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class CommandHeal implements CommandExecutor {
+
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        return false;
+    public boolean onCommand(@NotNull CommandSender cs, @NotNull Command cmd, @NotNull String s, @NotNull String[] args) {
+        Player p = (Player) cs;
+        if (args.length == 0) {
+            p.setHealth(p.getHealthScale());
+            p.setFoodLevel(20);
+        } else {
+            String healthStr = args[0];
+            Integer health = null;
+
+            try {
+                health = Integer.parseInt(healthStr);
+            } catch (NumberFormatException ignored) {}
+
+            if (health != null) {
+                p.setHealth(p.getHealth() + health);
+                p.setFoodLevel(20);
+            }
+
+        }
+
+        return true;
     }
 }
